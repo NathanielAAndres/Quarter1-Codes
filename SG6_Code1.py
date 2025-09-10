e712c1407609a84d8455a291e3c39c5dcad489d3
NathanielAAndres <johnnathaniel.andres@smc.pshs.edu.ph>
2025-09-10 08:58:16 +0800

    Addition of Three Numbers
    This problem indicates the addition of 3 numbers, with the final answer rounded off to two decimal places.

---
+++ SG6_Code1.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+sum = num1 + num2 + num3
+
+final = round(sum, 2)
+
+print("The total of the three numbers is:", final)
