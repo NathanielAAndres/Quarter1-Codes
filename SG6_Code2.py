63739d8c7c5c01a14738819aa80a95b216e86aac
NathanielAAndres <johnnathaniel.andres@smc.pshs.edu.ph>
2025-09-10 09:22:47 +0800

    Update SG6_Code2.py
    Edited the function by importing math.

--- SG6_Code2.py
+++ SG6_Code2.py
@@ -1,3 +1,6 @@
+import math
+
 radius = float(input("Enter the radius of the circle: "))
-area = 3.14 * (radius**2)
-print("The area of the circle is:", area)
+area = math.pi * pow(radius, 2)
+final = round(area, 2)
+print("The area of the circle is:", final)
